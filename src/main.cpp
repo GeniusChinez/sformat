52f764970f00a6bbb9d8bff31f7f8a60d4696f40
geniuschinez <geniuschinembiri@gmail.com>
2021-11-07 08:01:39 +0200

    Reduced indentation to 2 spaces

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 
 int main(int argc, char* const argv[]) {
-    auto result = gc::format("%: something % % %%", 1, "else", "is", true, '?');
-    std::cout << result << "\n";
-    return 0;
+  auto result = gc::format("%: something % % %%", 1, "else", "is", true, '?');
+  std::cout << result << "\n";
+  return 0;
 }
--- src/sformat.h
+++ src/sformat.h
@@ -6,38 +6,38 @@
 #include <sstream>
 
 namespace gc {
-    template <typename ...Args>
-    [[nodiscard]] 
-    std::string format(std::string_view fmt, Args&&... args) {
-        std::ostringstream ss;
-        auto format_iterator = fmt.begin();
+  template <typename ...Args>
+  [[nodiscard]] 
+  std::string format(std::string_view fmt, Args&&... args) {
+    std::ostringstream ss;
+    auto format_iterator = fmt.begin();
  
-        (void)std::initializer_list<int>{([&](const auto& arg) {
-            while (format_iterator != std::end(fmt)) {
-                if (*format_iterator == '\\') {
-                    if (*++format_iterator == '%') {
-                        ss << "%";
-                        format_iterator++;
-                    }
-                    else {
-                        ss << '\\';
-                    }
-                }
-                else if (*format_iterator == '%') {
-                    format_iterator++;
-                    ss << std::boolalpha << arg;
-                    return;
-                }
-                else {
-                    ss << *format_iterator++;
-                }
-            }
-        }(args), 0)...};
-
-        while (format_iterator != std::end(fmt)) {
-            ss << *format_iterator++;
+    (void)std::initializer_list<int>{([&](const auto& arg) {
+      while (format_iterator != std::end(fmt)) {
+        if (*format_iterator == '\\') {
+          if (*++format_iterator == '%') {
+            ss << "%";
+            format_iterator++;
+          }
+          else {
+            ss << '\\';
+          }
         }
+        else if (*format_iterator == '%') {
+          format_iterator++;
+          ss << std::boolalpha << arg;
+          return;
+        }
+        else {
+          ss << *format_iterator++;
+        }
+      }
+    }(args), 0)...};
 
-        return ss.str();
+    while (format_iterator != std::end(fmt)) {
+      ss << *format_iterator++;
     }
+
+    return ss.str();
+  }
 }
